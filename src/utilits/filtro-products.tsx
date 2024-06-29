dc1807c2d055f82e0fd8f3eee76e411a2c2e81a1
fritz-sport-ecommerce <166562372+fritz-sport-ecommerce@users.noreply.github.com>
2024-06-28 19:17:21 -0500

    Update filtro-products.tsx asd

--- src/utilits/filtro-products.tsx
+++ src/utilits/filtro-products.tsx
@@ -4,13 +4,13 @@ export function FiltroProducts(
   products: SanityProduct,
   razonsocial = "fritzsport"
 ) {
-  const productFilter = `_type == "product"   && categories != "originals" && images != undefined && name match "${products.name}*" && sku != "${products.sku}" && genero == "${products.genero}"`;
+  const productFilter = `_type == "product"   && categories != "originals" && images != undefined && name match "${products.name}*" && sku != "${products.sku}" && genero == "${products.genero}" && stock > 0 && stock != undefined`;
 
   return productFilter;
 }
 
 export function FiltroGlobal(razonsocial = "fritzsport") {
-  const productFilter = `_type == "product" && genero != undefined  && categories != "originals" && images != undefined  && images != null `;
+  const productFilter = `_type == "product" && genero != undefined  && categories != "originals" && images != undefined  && images != null && stock > 0 && stock != undefined`;
 
   return productFilter;
 }
@@ -19,7 +19,7 @@ interface Props {
   id?: string;
 }
 export function FiltroViewProduct(params: Props, razonsocial = "fritzsport") {
-  const productFilter = `*[_type == "product"   && categories != "originals" && slug.current == "${params.slug}" && sku == "${params.id}"][0]`;
+  const productFilter = `*[_type == "product"   && categories != "originals" && slug.current == "${params.slug}" && sku == "${params.id}"&& stock > 0 && stock != undefined][0]`;
 
   return productFilter;
 }
