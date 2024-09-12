9bb4b53f93c58a482f7bd17a1db16158adb06f50
fritz-sport-ecommerce <166562372+fritz-sport-ecommerce@users.noreply.github.com>
2024-09-12 11:53:35 -0500

    Update emprende.jsx

--- src/components/emprende/emprende.jsx
+++ src/components/emprende/emprende.jsx
@@ -167,6 +167,7 @@ export default function PaginaEmprende({ emprende }) {
                       </p>
                     </div>
                   ))}
+                 
                 </div>
                 {/* 
               /*-------------------------------------------*/
@@ -179,10 +180,11 @@ export default function PaginaEmprende({ emprende }) {
                     {emprende.tituloemprende}
                   </h4>
 
-                  <div>
+                  <div className="">
                     {emprende.emprendemayorista.map((el, i) => (
-                      <div key={el.titulo}>
-                        <div className="w-full flex justify-center py-8">
+                      <>
+                      <div key={el.titulo} className={`${i > 0 && "my-20"}`}>
+                        <div className="w-full flex justify-center py-20">
                           <div className="text-xl">
                             {i === 0 ? "EMPRENDEDOR" : "MAYORISTA"}
                           </div>
@@ -212,42 +214,72 @@ export default function PaginaEmprende({ emprende }) {
                                   </li>
                                 ))}
                               </ul>
+                                  <div className="mt-5">
+                                    {/*  */}
+                                    <div className="  text-center text-3xl font-bold  text-black dark:text-white">
+                                      {el.titulobeneficios}
+                                    </div>
+                                    <ul className="flex flex-col ">
+                                      {el.beneficios.map((el, i) => (
+                                        <li
+                                          key={i}
+                                          className="mt-3 font-semibold  text-black dark:text-white"
+                                        >
+                                          {el}
+                                        </li>
+                                      ))}
+                                    </ul>
+                                  </div>
                             </div>
 
-                            <div className="mt-10">
-                              {/*  */}
-                              <div className="  text-center text-3xl font-bold  text-black dark:text-white">
-                                {el.titulobeneficios}
-                              </div>
-                              <ul className="flex flex-col ">
-                                {el.beneficios.map((el, i) => (
-                                  <li
-                                    key={i}
-                                    className="mt-3 font-semibold  text-black dark:text-white"
-                                  >
-                                    {el}
-                                  </li>
-                                ))}
-                              </ul>
-                            </div>
-
-                                                    {emprende.pasos.empiezaahora.map((el, i) => (
-                          <a
-                            key={i}
-                            className="mr-5"
-                            href={`https://api.whatsapp.com/send/?phone=51${el.urlbutton}&text&type=phone_number&app_absent=0`}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <Button className="text-base rounded-none">
-                              {el.textbutton}
-                            </Button>
-                          </a>
-                        ))}
                           </div>
                         </div>
                       </div>
+                      <div className="my-10 flex h-3/5 w-full justify-center ">
+                                               <a
+                            
+                            className="mr-5"
+                            href={`https://api.whatsapp.com/send/?phone=51${emprende.pasos.empiezaahora[0].urlbutton}&text&type=phone_number&app_absent=0`}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <Button className="xl:text-lg text-base  flex items-center rounded-sm">
+                              <>
+                              
+                              EMPRENDE
+                              <svg
+            className="z h-6 w-6 ml-3 "
+            viewBox="0 0 30 30"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clipPath="url(#clip0_21_26)">
+              <circle cx="15.5" cy="15.5" r="11.5" fill="white" />
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M15.2219 0.578953C7.26327 0.578953 0.789474 7.05455 0.789474 15.0155C0.789474 18.1727 1.80764 21.1007 3.53846 23.4773L1.73964 28.841L7.28864 27.0675C9.57093 28.5782 12.2945 29.4522 15.2303 29.4522C23.189 29.4522 29.6628 22.9764 29.6628 15.0157C29.6628 7.05481 23.189 0.579191 15.2303 0.579191L15.2219 0.578953ZM11.1916 7.91206C10.9117 7.24163 10.6995 7.21624 10.2754 7.199C10.131 7.19062 9.97011 7.18224 9.79171 7.18224C9.23999 7.18224 8.66314 7.34344 8.31521 7.69986C7.89112 8.13268 6.83893 9.14252 6.83893 11.2135C6.83893 13.2844 8.34921 15.2873 8.55275 15.5676C8.76491 15.8473 11.4971 20.1588 15.7394 21.916C19.0569 23.2909 20.0413 23.1635 20.7963 23.0023C21.8993 22.7647 23.2824 21.9496 23.6303 20.9653C23.9783 19.9806 23.9782 19.1404 23.8762 18.9622C23.7745 18.784 23.4943 18.6824 23.0702 18.4699C22.6461 18.2577 20.5841 17.2392 20.1938 17.1034C19.8119 16.9592 19.4472 17.0103 19.1589 17.4177C18.7516 17.9863 18.3529 18.5636 18.0303 18.9114C17.7758 19.183 17.3598 19.217 17.0121 19.0726C16.5454 18.8776 15.2389 18.4189 13.6267 16.9846C12.3793 15.873 11.5309 14.4897 11.285 14.0739C11.0388 13.6497 11.2596 13.4032 11.4545 13.1742C11.6667 12.911 11.8702 12.7244 12.0824 12.4782C12.2945 12.2322 12.4133 12.1047 12.5491 11.8161C12.6935 11.5361 12.5915 11.2475 12.4897 11.0353C12.3879 10.823 11.5395 8.75208 11.1916 7.91206Z"
+                fill="#67C15E"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_21_26">
+                <rect
+                  width="28.8733"
+                  height="28.8733"
+                  transform="translate(0.789474 0.578953)"
+                />
+              </clipPath>
+            </defs>
+          </svg>                
+                              </>
+                            </Button>
+            </a>
+                      
+                    </div>
+                      </>
                     ))}
+        
                   </div>
 
                   {/* ---------------------------------------------------------------- */}
@@ -273,6 +305,7 @@ export default function PaginaEmprende({ emprende }) {
                             </p>
                           </div>
                         ))}
+       
                       </div>
                     </div>
                   </div>
@@ -303,6 +336,9 @@ export default function PaginaEmprende({ emprende }) {
                     </div>
                   </div>
                 </div>
+
+         
+
               </div>
             </div>
           </div>
