e684029c8895a9f356a64858039819e28dad6644
christian lozano <christian.lozano.job@gmail.com>
2025-02-21 14:28:36 -0500

    add skeleto catalogo pdf

--- src/app/(web)/pdf/page.tsx
+++ src/app/(web)/pdf/page.tsx
@@ -7,10 +7,7 @@ import { groq } from "next-sanity";
 
 import { SanityProduct } from "@/config/inventory";
 
-
-
 import { Metadata } from "next";
-
 
 import DescargarPdf from "@/components/pdf/DescargarPdf";
 
@@ -42,9 +39,6 @@ export const metadata: Metadata = {
   },
 };
 export default async function Page() {
-
-
-
   // console.log(productos);
 
   const catalogo = await client.fetch<SanityProduct[]>(
@@ -59,29 +53,25 @@ export default async function Page() {
   //   const [_file, id, extension] = pdf.split('-');
   //   return `https://cdn.sanity.io/files/ibvmpbc1/production/${id}.${extension}`
   // }
-  
+
   console.log(catalogo);
 
   // console.log(getUrlFromId());
 
-  
   return (
     <div>
-        {/* <object className='w-[100vw] h-[100vh]' data={getUrlFromId()} type="application/pdf"></object> */}
-
+      {/* <object className='w-[100vw] h-[100vh]' data={getUrlFromId()} type="application/pdf"></object> */}
 
       <div>
-      <div className="flex justify-center w-full 2xl:text-2xl xl:text-2xl mt-10 text-xl font-semibold">
-              CATALOGO
-              </div>
-      {/* <MainPdf catalogo={catalogo} items={products} /> */}
+        <div className="flex justify-center w-full 2xl:text-2xl xl:text-2xl mt-10 text-xl font-semibold">
+          CATALOGO
+        </div>
+        {/* <MainPdf catalogo={catalogo} items={products} /> */}
         <main className=" w-full px-6">
           <section
             aria-labelledby="products-heading"
             className="flex pb-24 pt-6"
           >
-       
-        
             {/* Product grid */}
             <DescargarPdf catalogo={catalogo}></DescargarPdf>
           </section>
