cd40ac6c0dcaa08dc3d4d133100315bd63c7570f
christian lozano <christian.lozano.job@gmail.com>
2025-09-19 16:02:13 -0500

    nueva version catalogo web3

--- src/app/(web)/libro-reclamaciones-virtual/page.tsx
+++
@@ -1,8 +0,0 @@
-import LibroReclamacionesForm from '@/components/libro-reclamaciones-virtual/libro-reclamaciones-virtual'
-import React from 'react'
-
-export default function LibroReclamacionesVirtual() {
-  return (
-    <LibroReclamacionesForm />
-  )
-}
